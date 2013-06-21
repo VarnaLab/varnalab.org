64fde8dbdf45d7c70df95c223da4335b48cb294c
Mihail Velikov <mihailvelik@gmail.com>
2013-06-21 22:24:01 +0300

    create add and list blogposs tests

--- tests/api/blogposts.spec.js
+++ tests/api/blogposts.spec.js
@@ -1,36 +1,53 @@
 describe("blogposts", function() {
 	var helpers = require('../helpers');
 	var request = require('request');
+	var dummyUser;
+	// fixture object that is used all over the test
+	var dummyBlogpost;
 
 	it('boots', function(next) {
-		helpers.boot(next);
+		helpers.boot(function() {
+			helpers.createUser(function(userData) {
+				dummyUser = userData;
+				dummyBlogpost = {
+					title : helpers.shortText,
+					member : dummyUser._id,
+					content : helpers.longText,
+					date : Date.now
+				};
+				next();
+			});
+			
+		});
 	});
 
-	// fixture object that is used all over the test
-	var dummyBlogpost = {
-		title : helpers.shortText,
-		memberId : '51bb2b7909a992281a00000f',
-		content : helpers.longText,
-		date : Date.now
-	};
-
-	it('add a blogpost', function(next) {
-		var post = dummyBlogpost;
-		post.title = null;
-
+	
+	it("add new blogpost", function(next){
+		blog = dummyBlogpost;
 		request.post({
-			uri: helpers.apiendpoint + "/blogposts/add",
-			json: post
-		}, function(err, res, body) {
-			console.log(body);
-			// expect(body.result).toBeDefined();
-			// expect(body.result._id).toBeDefined();
-			// expect(body.result._id).toMatch("^[0-9a-fA-F]{24}$");// A valid MongoId
-			// expect(body.result._id).toBe(post.memberId);
-
-			// expect(body.result.title).toBe(post.title);
-			// expect(body.result.content).toBe(post.content);
-			// expect(body.result.date).toBe(post.date);
+			uri: helpers.apiendpoint+"/blogposts/add",
+			json: blog
+		}, function(err, res, body){
+			expect(body.result).toBeDefined();
+			expect(body.result._id).toBeDefined();
+			expect(body.result._id).toBe == dummyUser._id;
+			next();
 		});
+	});
+
+	it('list all blogposts', function(next) {
+		request.get({
+			uri: helpers.apiendpoint + "/blogposts",
+			json: {}
+		}, function(err, res, body){
+			expect(body.result).toBeDefined();
+			expect(body.result).toBeArray();
+			expect(body.result.length).toBe == 1;
+			next();
+		})
+	});
+	
+	it('kill', function (){ 
+		helpers.kill();
 	});
 });
