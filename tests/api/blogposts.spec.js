737f7abeb542b30abaf8b48b193c20acfe0de411
Mihail Velikov <mihailvelik@gmail.com>
2013-06-22 19:15:59 +0300

    add new test that checks for a valid member for every blogpost

--- tests/api/blogposts.spec.js
+++ tests/api/blogposts.spec.js
@@ -40,11 +40,28 @@ describe("blogposts", function() {
 			uri: helpers.apiendpoint + "/blogposts",
 			json: {}
 		}, function(err, res, body){
+			// console.log(body);
 			expect(body.result).toBeDefined();
 			expect(body.result).toBeArray();
-			expect(body.result.length).toBe == 1;
+			expect(body.result.length).toBe === 1;
+			expect(body.result[0].title).toBe === blog.title;
+			expect(body.result[0].content).toBe === blog.content;
+			expect(body.result[0].date).toBe === blog.date;
 			next();
 		})
+	});
+
+	it('reject inserting blogpost without a valid member', function (next) {
+		blog = dummyBlogpost;
+		blog.member = null;
+
+		request.post({
+			uri:helpers.apiendpoint + '/blogposts/add',
+			json: blog
+		}, function (err, res, body) {
+			console.log(err, body);
+			next();
+		});
 	});
 	
 	it('kill', function (){ 
