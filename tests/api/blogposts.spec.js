c25ffd4e3991c5d01c6546864f9e22811289ef2b
Mihail Velikov <mihailvelik@gmail.com>
2013-06-27 23:39:19 +0300

    add undescore module in blogpost test; fix test cases

--- tests/api/blogposts.spec.js
+++ tests/api/blogposts.spec.js
@@ -1,6 +1,7 @@
 describe("blogposts", function() {
   var helpers = require('../helpers');
   var request = require('request');
+  var _ = require('underscore');
   var dummyUser;
   // fixture object that is used all over the test
   var dummyBlogpost;
@@ -23,7 +24,7 @@ describe("blogposts", function() {
 
   
   it("add new blogpost", function(next){
-    blog = dummyBlogpost;
+    var blog = _.clone(dummyBlogpost);
     request.post({
       uri: helpers.apiendpoint+"/blogposts/add",
       json: blog
@@ -39,21 +40,21 @@ describe("blogposts", function() {
       uri: helpers.apiendpoint + "/blogposts",
       json: {}
     }, function(err, res, body){
-      var blogDate = new Date(blog.date).getTime(),
+      var blogDate = new Date(dummyBlogpost.date).getTime(),
       resultBlogDate = new Date(body.result[0].date).getTime();
 
       expect(body.result).toBeDefined();
       expect(body.result).toBeArray();
       expect(body.result.length).toBe(1);
-      expect(body.result[0].title).toBe(blog.title);
-      expect(body.result[0].content).toBe(blog.content);
+      expect(body.result[0].title).toBe(dummyBlogpost.title);
+      expect(body.result[0].content).toBe(dummyBlogpost.content);
       expect(resultBlogDate).toBe(blogDate);
       next();
     })
   });
 
   it('reject inserting blogpost without a valid member', function (next) {
-    blog = dummyBlogpost;
+    var blog = _.clone(dummyBlogpost);
     blog.member = null;
 
     request.post({
@@ -63,13 +64,13 @@ describe("blogposts", function() {
 
       expect(body.result.errors).toBeDefined();
       expect(body.result.message).toBe('Validation failed');
-      expect(body.result.errors.member.message).toBe('Validator "required" failed for path member');
+      expect(body.result.errors.member.message).toBe('Invalid member id provided');
       next();
     });
   });
 
   it('reject inserting blogpost with an invalid member', function (next) {
-    blog = dummyBlogpost;
+    var blog = _.clone(dummyBlogpost);
     blog.member = "test";
 
     request.post({
@@ -79,19 +80,20 @@ describe("blogposts", function() {
 
       expect(body.result.errors).toBeDefined();
       expect(body.result.message).toBe('Validation failed');
-      expect(body.result.errors.member.message).toBe('Validator "required" failed for path member');
+      expect(body.result.errors.member.message).toBe('Invalid member id provided');
       next();
     });
   });
 
   it('reject inserting blogpost without a valid content', function (next) {
-    blog = dummyBlogpost;
+    var blog = _.clone(dummyBlogpost);
     blog.content = null;
 
     request.post({
       uri : helpers.apiendpoint + "/blogposts/add",
       json : blog
     }, function (err, res, body) {
+      // console.log(body.result);
       expect(body.result).toBeDefined();
       expect(body.result.errors).toBeDefined();
       expect(body.result.message).toBe('Validation failed');
@@ -99,6 +101,23 @@ describe("blogposts", function() {
       next();
     });
   });
+
+  it('reject inserting blogpost without a valid date', function(next) {
+    var blog = _.clone(dummyBlogpost);
+    blog.date = null;
+
+    request.post({
+      uri : helpers.apiendpoint + "/blogposts/add",
+      json : blog
+    }, function(err, res, body) {
+      expect(body.result).toBeDefined();
+      expect(body.result.errors).toBeDefined();
+      expect(body.result.message).toBe('Validation failed');
+      expect(body.result.errors.date.message).toBe('Validator "required" failed for path date');
+      next();
+    });
+  });
+
   it('kill', function (){ 
     helpers.kill();
   });
