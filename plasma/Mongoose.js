430c23f2135a97a10b65da21e7b4b0c18c8a00cf
Kamen Naydenov <pau4o@kamennn.eu>
2013-09-12 21:01:56 +0300

    MongoDB is gone
    Exit with error when MongoDB is missing

--- plasma/Mongoose.js
+++ plasma/Mongoose.js
@@ -3,14 +3,14 @@ var mongoose = require("mongoose");
 
 module.exports = Organel.extend(function Mongoose(plasma, config){
   Organel.call(this, plasma);
-  
+
   this.config = config;
 
   if(config.reactOn)
     this.on(config.reactOn, this.connect)
   else
     this.connect()
-  
+
   plasma.on("kill", function(){
     mongoose.disconnect();
     return false;
@@ -18,11 +18,17 @@ module.exports = Organel.extend(function Mongoose(plasma, config){
 }, {
   connect: function(c){
     var self = this;
-    mongoose.connect('localhost', self.config.database.name, function(){
+    mongoose.connect('localhost', self.config.database.name, function(err){
+      if (err) {
+        console.log("\033[2J"); // clear screen
+        console.error("Please start MongoDB!".red);
+        process.exit();
+      }
+
       if(self.config.recreateDatabase) {
         mongoose.connection.db.dropDatabase(function(){
           mongoose.connect('localhost', self.config.database.name, function(){
-            self.emit({type: "Mongoose", data:{}});  
+            self.emit({type: "Mongoose", data:{}});
           });
         });
       } else {
