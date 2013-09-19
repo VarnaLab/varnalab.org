6c80675e3c55e5c1f637c2992a3731c6871250c6
Boris Filipov <obiwonn@gmail.com>
2013-09-20 01:20:03 +0300

    shown blogs posts from newest to oldest

--- context/http/helpers/getAllBlogs.js
+++ context/http/helpers/getAllBlogs.js
@@ -4,12 +4,16 @@ module.exports = function(pageNumberParamName, limit){
   return function(req, res, next) {
     var pageNumber = req.params[pageNumberParamName]?parseInt(req.params[pageNumberParamName]):0
     var skip = pageNumber*limit
-    BlogPost.find({}).populate("creator").limit(limit).skip(skip).exec(function(err, blogs){
-      if(err) return next(err);
-      req.blogs = blogs;
-      req.pageNumber = pageNumber
-      req.blogsLimit = limit
-      next();
-    })
+    BlogPost.find({}).populate("creator")
+      .limit(limit)
+      .skip(skip)
+      .sort({date: -1})
+      .exec(function(err, blogs){
+        if(err) return next(err);
+        req.blogs = blogs;
+        req.pageNumber = pageNumber
+        req.blogsLimit = limit
+        next();
+      })
   }
 }
