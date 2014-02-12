09eecc6fc1ffa09f1f7dee948675533003a2e698
Boris Filipov <obiwonn@gmail.com>
2014-02-12 20:52:45 +0200

    fix blogposts importing to not duplicate entries

--- scripts/import-blogposts/index.js
+++ scripts/import-blogposts/index.js
@@ -15,23 +15,27 @@ module.exports.exec = function(next) {
         sanitize: false
       })
       var allCount = 0;
-      for(var i = 0;  i< xml.feed.entry.length; i++) {
-        var entry = xml.feed.entry[i]
+      xml.feed.entry.forEach(function(entry){
         if(entry.category.term.indexOf("kind#post") !== -1) {
           allCount += 1
-          BlogPost.create({
-            creator: admin,
-            created: new Date(entry.published),
-            updated: new Date(entry.updated),
-            title: entry.title.$t,
-            slug: entry.title.$t,
-            content: entry.content.$t
-          }, function(err, post){
-            console.log(entry, err, post)
+          BlogPost.findOne({
+            title: entry.title.$t
+          }, function(err, blog){
+            if(blog) return
+            BlogPost.create({
+              creator: admin,
+              created: new Date(entry.published),
+              updated: new Date(entry.updated),
+              title: entry.title.$t,
+              slug: entry.title.$t,
+              content: entry.content.$t
+            }, function(err, post){
+              console.log("Inserted", entry.title.$t, allCount)
+            })
           })
         }
-      }
-      console.log(allCount)
+      })
+      console.log("Will insert total", allCount)
     })
   })
 }
