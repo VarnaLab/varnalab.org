7c5266415347d590bada3a1e788b94c1a528d024
Boris Filipov <obiwonn@gmail.com>
2014-02-27 20:22:33 +0200

    set shelljs as part of deps as it is required for building of client side
    * fix release script

--- scripts/release-staging.js
+++ scripts/release-staging.js
@@ -1,5 +1,13 @@
 require("shelljs/global");
-if(exec("node ./node_modules/organic-angel/bin/angel cell upgrade ./dna/staging").code != 0){
+if(exec("node ./node_modules/organic-angel/bin/angel cell upgrade ./dna/staging.json").code != 0){
   echo("Error: failed to upgrade staging");
+  exit(1);
+}
+if(exec("node ./node_modules/organic-angel/bin/angel cell build ./dna/staging.json").code != 0){
+  echo("Error: failed to build staging");
+  exit(1);
+}
+if(exec("node ./node_modules/organic-angel/bin/angel cell restart ./dna/staging.json").code != 0){
+  echo("Error: failed to restart staging");
   exit(1);
 }
