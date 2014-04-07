96780b79fb699796437af22d9408c6d74bd9c1d5
Boris Filipov <obiwonn@gmail.com>
2014-04-07 23:27:07 +0300

    fix production assets destination and upgrade-production script

--- scripts/upgrade-production.js
+++ scripts/upgrade-production.js
@@ -1,13 +1,13 @@
 require("shelljs/global");
-if(exec("node ./node_modules/organic-angel/bin/angel cell upgrade ./dna/production.json").code != 0){
+if(exec("node ./node_modules/organic-angel/bin/angel cell upgrade ./dna/production/cell.json").code != 0){
   echo("Error: failed to upgrade production");
   exit(1);
 }
-if(exec("node ./node_modules/organic-angel/bin/angel cell build ./dna/production.json").code != 0){
+if(exec("node ./node_modules/organic-angel/bin/angel cell build ./dna/production/cell.json").code != 0){
   echo("Error: failed to build production");
   exit(1);
 }
-if(exec("node ./node_modules/organic-angel/bin/angel cell restart ./dna/production.json").code != 0){
+if(exec("node ./node_modules/organic-angel/bin/angel cell restart ./dna/production/cell.json").code != 0){
   echo("Error: failed to restart production");
   exit(1);
 }
