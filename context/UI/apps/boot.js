09018aae2c0dab1963e8ffbe539f2166957f984d
Boris Filipov <obiwonn@gmail.com>
2014-07-29 19:24:02 +0300

    fix Backbone.$ missing reference to jquery, fix admin css link, always provide disqus config data to page routes

--- context/UI/apps/boot.js
+++ context/UI/apps/boot.js
@@ -5,4 +5,5 @@ require("./vendor/bootstrap.min");
 _ = require("./vendor/underscore");
 moment = require("./vendor/moment");
 Backbone = require("./vendor/backbone");
+Backbone.$ = $;
 config = require("dna");
