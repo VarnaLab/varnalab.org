a3414f8df213ec16849a19ca68ec444e18c91824
outbounder <obiwonn@gmail.com>
2015-11-04 21:25:33 +0200

    fixed /events listing and introduced upcoming and past sections of events

--- context/http/helpers/getAllEvents.js
+++ context/http/helpers/getAllEvents.js
@@ -5,21 +5,25 @@ var moment = require("moment");
 var sortByStartDateTime = function(e){return e.startDateTime}
 
 var orderUpcomingFirstDesc = function(events) {
-  var upcoming = _.filter(events, function(e){ 
+  var upcoming = _.filter(events, function(e){
     return moment(e.startDateTime).diff(moment()) > 0
   })
-  var rest = _.filter(events, function(e){ 
+  return _.sortBy(upcoming, sortByStartDateTime)
+}
+
+var orderPastRecentDesc = function (events) {
+  var past = _.filter(events, function(e){
     return moment(e.startDateTime).diff(moment()) < 0
   })
-  return _.sortBy(upcoming, sortByStartDateTime)
-    .concat(_.sortBy(rest, sortByStartDateTime))
+  return _.sortBy(past, sortByStartDateTime).reverse()
 }
 
 module.exports = function(req, res, next) {
-  Event.find({}).populate("creator").sort({startDateTime: 1}).exec(function(err, events){
+  Event.find({}).populate("creator").sort({startDateTime: -1}).exec(function(err, events){
     if(err) return next(err);
     res.locals.events = events;
     res.locals.eventsUpcomingOrder = orderUpcomingFirstDesc(events)
+    res.locals.pastEvents = orderPastRecentDesc(events)
     next();
   })
 }
