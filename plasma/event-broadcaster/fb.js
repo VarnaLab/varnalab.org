89ed13235e2213b7413ab05994ae78977407ec8e
outbounder <obiwonn@gmail.com>
2016-03-26 01:21:22 +0200

    use only link when broadcasting to fb

--- plasma/event-broadcaster/fb.js
+++ plasma/event-broadcaster/fb.js
@@ -8,18 +8,7 @@ var FB = function (dna) {
 FB.prototype.exec = function (event, user) {
   var dna = this.dna
 
-  var notificationText = ''
-  notificationText += [
-    event.title + ', ' + event.startDate() + ' ' + event.startTime(),
-    '',
-    dna.fronturls.eventsPage + event.getUrlEncoded(),
-    '----',
-    '',
-    'Автоматично съобщение изпратено от varnalab.org'
-  ].join('\n')
-
   fb.api("/121769877975286/feed", "POST", {
-    "message": notificationText,
     "link": dna.fronturls.eventsPage + event.getUrl(),
     "access_token": user.fbauth.access_token
   }, function (response) {
