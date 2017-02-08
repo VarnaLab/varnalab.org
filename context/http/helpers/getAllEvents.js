cdb437daad8c7f6bddd26b4d526efad492c0bdfa
outbounder <obiwonn@gmail.com>
2017-02-08 22:12:50 +0200

    make past events paginated on /events page

--- context/http/helpers/getAllEvents.js
+++ context/http/helpers/getAllEvents.js
@@ -18,12 +18,22 @@ var orderPastRecentDesc = function (events) {
   return _.sortBy(past, sortByStartDateTime).reverse()
 }
 
-module.exports = function(req, res, next) {
-  Event.find({}).populate("creator").sort({startDateTime: -1}).exec(function(err, events){
-    if(err) return next(err);
-    res.locals.events = events;
-    res.locals.eventsUpcomingOrder = orderUpcomingFirstDesc(events)
-    res.locals.pastEvents = orderPastRecentDesc(events)
-    next();
-  })
+module.exports = function (pageNumberParamName, limit) {
+  return function(req, res, next) {
+    var pageNumber = req.params[pageNumberParamName]?parseInt(req.params[pageNumberParamName]):0
+    var skip = pageNumber*limit
+    Event.find({}).populate("creator")
+      .sort({startDateTime: -1})
+      .exec(function(err, events){
+        if(err) return next(err);
+        res.locals.events = events;
+        res.locals.eventsUpcomingOrder = orderUpcomingFirstDesc(events)
+        res.locals.pastEvents = orderPastRecentDesc(events)
+        res.locals.pastEventsCount = res.locals.pastEvents.length
+        res.locals.pastEvents = res.locals.pastEvents.splice(skip, limit)
+        res.locals.pastEventsLimit = limit
+        res.locals.pageNumber = pageNumber
+        next();
+      })
+  }
 }
